fd9d01ddfb9c30d7e311a7bc39728cc20dad314e
phamthaihoang1234 <70738519+phamthaihoang1234@users.noreply.github.com>
2021-04-19 17:16:42 +0700

    add hook random change color

--- src/hooks/useMagicColor.js
+++ src/hooks/useMagicColor.js
@@ -1,22 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 
-function randomColor() {
+function randomColor(currentColor) {
     const COLOR_LIST = ['red', 'green','yellow'];
     //random 0 -> 2
-    const randomIndex = Math.trunc(Math.random() *3);
+    const currentIndex = COLOR_LIST.indexOf(currentColor);
+    let newIndex = currentIndex;
+    
+    while(currentIndex === newIndex){
+        newIndex = Math.trunc(Math.random() *3);
+    }
 
-    console.log(COLOR_LIST[randomIndex]);
-    return COLOR_LIST[randomIndex];
+    console.log(COLOR_LIST[newIndex]);
+    return COLOR_LIST[newIndex];
 }
 function useMagicColor(props) {
     const [color, setColor] = useState('transparent');
+    const colorRef = useRef('transparent');
 
     // change color every 1 second
     useEffect(() => {
         const colorInterval = setInterval(() => {
-                const newColor = randomColor();
+            // console.log('First color: ',color);
+            console.log('First color: ',colorRef.current);
+                const newColor = randomColor(colorRef.current);
                 setColor(newColor);
+
+                colorRef.current = newColor;
         },1000);
 
         return () => {
